12238e7279bd5fee93ed333e38ce3d8e958c01f9
RenataPilka <renata.pilka@gmail.com>
2024-10-18 19:04:13 +0300

    Improving Project

--- Projektas/script.js
+++ Projektas/script.js
@@ -1,4 +1,18 @@
-// item 
-// width: 20%; 
-//     height: auto;
-//     text-align: center;
+window.onscroll = function() {
+    const goUpBtn = document.getElementById('goUpBtn');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        goUpBtn.style.display = "block"; // Show button
+    } else {
+        goUpBtn.style.display = "none"; // Hide button
+    }
+};
+
+function goTop() {
+
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Smooth scroll
+    });
+
+}
+
